6fe2e8481a8c5d2433b4d11ba82da8ecf87830d1
六翁 <lu.hl@alibaba-inc.com>
2021-04-28 17:47:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## hello websocket
+
+```bash
+mvn spring-boot:run
+```
+
+```bash
+open http://localhost:8080
+```
+
+<img src="test.png" style="width:500px;" />
+
---
+++ src/main/java/com/example/WsApi.java
@@ -0,0 +1,48 @@
+package com.example;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author 六翁 lu.hl@alibaba-inc.com
+ * @date 2017/10/01
+ */
+@Controller
+@EnableScheduling
+public class WsApi {
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @MessageMapping("/send")
+    @SendTo("/topic/send")
+    public SocketMessage send(SocketMessage message) throws Exception {
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        message.date = df.format(new Date());
+        message.message = ">>" + message.message;
+        return message;
+    }
+
+    @Scheduled(fixedRate = 1000)
+    @SendTo("/topic/callback")
+    public Object callback() throws Exception {
+        // 发现消息
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        messagingTemplate.convertAndSend("/topic/callback", df.format(new Date()));
+        return "callback";
+    }
+}
